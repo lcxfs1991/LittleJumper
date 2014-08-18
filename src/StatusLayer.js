fe45fd9598fe5c2f10a307948f752833d98de306
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2014-08-18 14:03:51 +0800

    change status layout

--- src/StatusLayer.js
+++ src/StatusLayer.js
@@ -24,28 +24,28 @@ var StatusLayer = cc.Layer.extend({
     init:function () {
         this._super();
 
-        _level = new Array();
-        _level[0] = "大一";
-        _level[1] = "大二";
-        _level[2] = "大三";
-        _level[3] = "大四";
-        _level[4] = "精英";
+//        _level = new Array();
+//        _level[0] = "大一";
+//        _level[1] = "大二";
+//        _level[2] = "大三";
+//        _level[3] = "大四";
+//        _level[4] = "精英";
 
-        this.currentLevel = _level[0];
+//        this.currentLevel = _level[0];
 
         var winsize = cc.director.getWinSize();
 
-        this.labelLevel = cc.LabelTTF.create("Level: "+this.currentLevel, "Helvetica", 32);
+//        this.labelLevel = cc.LabelTTF.create("Level: "+this.currentLevel, "Helvetica", 32);
 //        this.labelCoin.setColor(cc.color(0,0,0));//black color
-        this.labelLevel.setPosition(cc.p(110, winsize.height - 15));
-        this.addChild(this.labelLevel);
+//        this.labelLevel.setPosition(cc.p(110, winsize.height - 15));
+//        this.addChild(this.labelLevel);
 
         this.number = 0.0;
 
         var labelName = ""+this.number;
         _labelNumber = cc.LabelTTF.create(labelName, "Arial", 32);
-//        _labelNumber.setColor(cc.c3(64, 64, 64));
-        _labelNumber.setPosition(cc.p(winsize.width - 70, winsize.height - 15));
+        _labelNumber.setColor(cc.color(0, 0, 0));
+        _labelNumber.setPosition(cc.p(winsize.width - 70, winsize.height - 20));
 
         _updateRate = 0.1;
 
@@ -58,33 +58,33 @@ var StatusLayer = cc.Layer.extend({
 
         this.currentStep += num;
 
-        if (this.currentStep < 15){
-            this.currentLevel = _level[0];
-        }
-        else if (this.currentStep >= 15 && this.currentStep < 30)
-        {
-            this.currentLevel = _level[1];
-        }
-        else if (this.currentStep >= 30 && this.currentStep < 45)
-        {
-            this.currentLevel = _level[2];
-        }
-        else if (this.currentStep >= 45 && this.currentStep < 70)
-        {
-            this.currentLevel = _level[2];
-        }
-        else if (this.currentStep >= 70 && this.currentStep < 90)
-        {
-            this.currentLevel = _level[3];
-        }
-        else if (this.currentStep >= 90)
-        {
-            this.currentLevel = _level[4];
-        }
-
-        this.removeChild(this.labelLevel);
-        this.labelLevel.setString("Level: "+this.currentLevel);
-        this.addChild(this.labelLevel);
+//        if (this.currentStep < 15){
+//            this.currentLevel = _level[0];
+//        }
+//        else if (this.currentStep >= 15 && this.currentStep < 30)
+//        {
+//            this.currentLevel = _level[1];
+//        }
+//        else if (this.currentStep >= 30 && this.currentStep < 45)
+//        {
+//            this.currentLevel = _level[2];
+//        }
+//        else if (this.currentStep >= 45 && this.currentStep < 70)
+//        {
+//            this.currentLevel = _level[2];
+//        }
+//        else if (this.currentStep >= 70 && this.currentStep < 90)
+//        {
+//            this.currentLevel = _level[3];
+//        }
+//        else if (this.currentStep >= 90)
+//        {
+//            this.currentLevel = _level[4];
+//        }
+//
+//        this.removeChild(this.labelLevel);
+//        this.labelLevel.setString("Level: "+this.currentLevel);
+//        this.addChild(this.labelLevel);
 
         return this.currentStep;
 
